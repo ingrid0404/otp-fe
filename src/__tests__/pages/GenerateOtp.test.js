56c4ef128f1bbb3e3a2b524cb80b8fd98c209177
ingrid68456 <113686835+ingrid0404@users.noreply.github.com>
2023-12-17 13:52:52 +0200

    add unit tests

--- src/__tests__/pages/GenerateOtp.test.js
+++ src/__tests__/pages/GenerateOtp.test.js
@@ -1,7 +1,10 @@
+/* eslint-disable testing-library/no-wait-for-side-effects */
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import GenerateOTP from "../../pages/GenerateOTP";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from '@testing-library/user-event';
+
 
 describe("<GenerateOTP />", () => {
   it("renders correclty", () => {
@@ -13,5 +16,31 @@ describe("<GenerateOTP />", () => {
     expect(screen.getByText(/Let's make sure it's you:/i)).toBeTruthy();
     expect(screen.getByText(/You will receive an OTP by email/i)).toBeTruthy();
     expect(screen.getByRole("button", { name: /Submit/i })).toBeTruthy();
+  });
+
+  it("Add correct email", async () => {
+    render(
+      <BrowserRouter>
+        <GenerateOTP />
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByRole("button", { name: /Submit/i })).toBeDisabled();
+    expect(screen.getByRole('textbox', { name: "" })).toBeTruthy();
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), 'JavaScript@gmai.com'));
+    expect(screen.getByRole("button", { name: /Submit/i })).toBeEnabled();
+  });
+
+  it("Add incorrect email", async () => {
+    render(
+      <BrowserRouter>
+        <GenerateOTP />
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByRole("button", { name: /Submit/i })).toBeDisabled();
+    expect(screen.getByRole('textbox', { name: "" })).toBeTruthy();
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), 'JavaScript'));
+    expect(screen.getByRole("button", { name: /Submit/i })).toBeDisabled();
   });
 });
