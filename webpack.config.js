ecb978da29895f6fe6190ee3af760d9744126d18
ingrid <dianaingridtakacs@gmail.com>
2024-01-26 20:16:09 +0200

    add babel with webpack

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                }
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', "sass-loader"]
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+        }
+    ]
+    }
+};
+
+/*
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js'
+   },
+   devServer: {
+      port: 8080
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+   },
+   plugins:[
+       new HtmlWebpackPlugin({
+            template: path.join(__dirname,'/src/index.html')
+       }) 
+   ]
+}
+*/
